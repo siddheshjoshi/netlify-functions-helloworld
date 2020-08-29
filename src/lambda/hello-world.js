a159e1c31c1e3df42ee45dced0e6a800202a6f45
Siddhesh Joshi <siddheshmail@gmail.com>
2020-08-29 11:46:48 -0400

    Updated to use queryStringParameters

--- src/lambda/hello-world.js
+++ src/lambda/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: "<h1>Hello World</h1>",
+    body: "Hello " + event.queryStringParameters.name,
   });
 };
