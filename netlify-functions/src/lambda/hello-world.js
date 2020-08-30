29741180fcfca87aad542a31ec749af248e77ea0
Siddhesh Joshi <siddheshmail@gmail.com>
2020-08-30 19:48:15 -0400

    Update hello-world.js

--- netlify-functions/src/lambda/hello-world.js
+++ netlify-functions/src/lambda/hello-world.js
@@ -1,6 +1,10 @@
-exports.handler = function (event, context, callback) {
-  callback(null, {
+exports.handler = function(event, context, callback) {
+  const headers = callback(null, {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+    },
     body: "Hello " + event.queryStringParameters.name,
   });
   //callback(error, success)
